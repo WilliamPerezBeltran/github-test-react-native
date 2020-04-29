4fbfe76cb61f424ce8119d17a449b6e3491333b7
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-04-29 13:18:54 -0500

    add axios and fetch data

--- js/components/HomeScreen.js
+++ js/components/HomeScreen.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
 import { View, Text, Button, StyleSheet } from 'react-native';
+import * as FetchData from "../services/FetchGists";
 
-const HomeScreen = ({navigation}) => {
-    return (
+class HomeScreen extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+            data: {},
+        };
+    }
+
+   componentDidMount = async () => {
+          const fetchedData = await FetchData.FetchAllGist();
+          console.log(fetchedData)
+          this.setState({ data: fetchedData });
+    };
+
+  render() {
+     return (
       <View style={styles.container}>
         <Text>Home Screen</Text>
       <Button
         title="Go to details screen"
-        onPress={() => navigation.navigate("Details")}
+        onPress={() => this.props.navigation.navigate("Details")}
       />
       </View>
     );
-};
+  }
+}
 
 export default HomeScreen;
 
@@ -22,3 +38,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
 });
+
+// const HomeScreen = ({navigation}) => {
+
+//     return (
+//       <View style={styles.container}>
+//         <Text>Home Screen</Text>
+//       <Button
+//         title="Go to details screen"
+//         onPress={() => navigation.navigate("Details")}
+//       />
+//       </View>
+//     );
+// };
+
+// export default HomeScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1, 
+//     alignItems: 'center', 
+//     justifyContent: 'center'
+//   },
+// });
