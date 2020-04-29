fcb1fdbbca3744cc4e78ba6e14ca03c96aee018e
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2020-04-29 14:01:29 -0500

    add style HomeScreen

--- js/components/HomeScreen.js
+++ js/components/HomeScreen.js
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {
+    Platform,
+    Text,
+    View,
+    Button,
+    StyleSheet,
+    TextInput,
+    Dimensions,
+    TouchableOpacity,
+    ScrollView,
+    TouchableHighlight,
+    Modal,
+    AsyncStorage,
+    TouchableWithoutFeedback,
+    ToastAndroid,
+    Alert
+} from "react-native";
 import * as FetchData from "../services/FetchGists";
+
+var dimensions = {
+    width: Dimensions.get("window").width / 1.5,
+};
 
 class HomeScreen extends Component {
   constructor(props) {
@@ -19,11 +39,44 @@ class HomeScreen extends Component {
   render() {
      return (
       <View style={styles.container}>
-        <Text>Home Screen</Text>
-      <Button
-        title="Go to details screen"
-        onPress={() => this.props.navigation.navigate("Details")}
-      />
+        <ScrollView>
+          <View style={styles.InnerContainer}>
+              <Text style={styles.Title}>Pagar recibo</Text>
+              <View
+                  style={[
+                      styles.Card,
+                      { width: dimensions.width }
+                  ]}
+              >
+                  <View>
+                      <View style={styles.containerText}>
+                          <Text style={styles.SubTitleCard}>
+                              Valor del recibo a pagar:
+                          </Text>
+                          <Text>
+                              text
+                          </Text>
+                      </View>
+                  </View>
+
+                  <View>
+                      <View style={styles.containerText}>
+                          <Text style={styles.SubTitleCard}>
+                              Tipo del recibo:
+                          </Text>
+                          <Text>
+                              edeed
+                          </Text>
+                      </View>
+                  </View>
+              </View>
+          </View>
+        </ScrollView>
+          <Text>Home Screen</Text>
+        <Button
+          title="Go to details screen"
+          onPress={() => this.props.navigation.navigate("Details")}
+        />
       </View>
     );
   }
@@ -32,12 +85,57 @@ class HomeScreen extends Component {
 export default HomeScreen;
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1, 
     alignItems: 'center', 
     justifyContent: 'center'
-  },
+  },  
+  InnerContainer: {
+        flex: 1,
+        alignItems: "center"
+    },
+     Title: {
+        color: "black",
+        fontSize: 19,
+        marginTop: "5%",
+        marginBottom: "10%"
+    },
+     Card: {
+        borderWidth: 1.5,
+        borderColor: "#B8B8B8",
+        borderRadius: 5,
+        shadowColor: "black",
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 10,
+        elevation: 3,
+        backgroundColor: "#fff",
+        marginBottom: 20
+    },
+    containerText: {
+        borderColor: "#B8B8B8",
+        borderBottomWidth: 1.5,
+        paddingTop: 10,
+        paddingLeft: 10,
+        paddingBottom: 10,
+        flexDirection: "row"
+    },
+    SubTitleCard: {
+        width: 100,
+        color: "#3F51B5",
+        fontSize: 15,
+        fontWeight: "400"
+    },
 });
+
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1, 
+//     alignItems: 'center', 
+//     justifyContent: 'center'
+//   },
+// });
 
 // const HomeScreen = ({navigation}) => {
 
